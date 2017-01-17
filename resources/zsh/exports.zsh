729beb7524baa187d5aeff6235aa222f4ca72d91
Kyle DeTella <kdetella@gmail.com>
2017-01-17 15:30:32 +0000

    Remove GREP_OPTIONS

--- resources/zsh/exports.zsh
+++ resources/zsh/exports.zsh
@@ -11,7 +11,6 @@ export CLICOLOR=1
 # export LS_COLORS='di=34;40:ln=35;40:so=32;40:pi=33;40:ex=0;40:bd=34;46:cd=34;43:su=0;41:sg=0;46:tw=0;42:ow=0;43:'
 # export LSCOLORS='exfxcxdxaxegedabagacad'
 
-export GREP_OPTIONS='--color=auto'
 export GREP_COLOR='00;36'
 
 export PYTHONSTARTUP="$HOME/.pythonrc"
